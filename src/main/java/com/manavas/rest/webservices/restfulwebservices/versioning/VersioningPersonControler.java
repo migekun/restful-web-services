f995c0bab87c1f56bef64849eb75fea57741ddac
Miguel Ángel Navas García <manavas@gmail.com>
2023-10-03 18:46:02 +0200

    Versioning url

--- src/main/java/com/manavas/rest/webservices/restfulwebservices/versioning/VersioningPersonControler.java
+++ src/main/java/com/manavas/rest/webservices/restfulwebservices/versioning/VersioningPersonControler.java
@@ -14,4 +14,35 @@ public class VersioningPersonControler {
     public PersonV2 getSecondVersionOfPerson() {
         return new PersonV2(new Name("Miguel", "Navas"));
     }
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
+        return new PersonV1("Miguel Navas Params request");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParam() {
+        return new PersonV2(new Name("Miguel", "Navas"));
+    }
+
+    @GetMapping(path = "/person/headers", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonHeaders() {
+        return new PersonV1("Miguel Navas");
+    }
+
+    @GetMapping(path = "/person/headers", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonHeader() {
+        return new PersonV2(new Name("Miguel", "Navas"));
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAccept() {
+        return new PersonV1("Miguel Navas");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfAcceot() {
+        return new PersonV2(new Name("Miguel", "Navas"));
+    }
+
 }
